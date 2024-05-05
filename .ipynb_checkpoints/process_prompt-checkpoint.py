3f846c133a58c66fa2694964c75060f212fdd685
Pranav <pranavkumarkota@gmail.com>
2024-05-05 01:46:16 +0000

    get prompt embedding

---
+++ .ipynb_checkpoints/process_prompt-checkpoint.py
@@ -0,0 +1,83 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from huggingface_hub import login
+from hf_token import hf_token
+from utils.lora_ckpt import load_lora
+from peft import LoraConfig, get_peft_model
+import sys
+import pickle
+import time
+
+
+access_token = hf_token
+login(token=access_token)
+
+#Load the tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token=access_token)
+tokenizer.pad_token = '[PAD]'
+model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", token=access_token)
+
+
+
+
+lora_modules = ['q_proj', 'v_proj']
+lora_r = 16
+lora_alpha = lora_r
+lora_dropout = 0.05
+
+
+lora_config = LoraConfig(
+        r = lora_r,
+        lora_alpha = lora_alpha,
+        target_modules = lora_modules,
+        lora_dropout = lora_dropout,
+        bias = "none"
+)
+
+weights_path = './weights/lora_llm.ckpt'
+
+lora_llm = get_peft_model(model=model, peft_config=lora_config)
+load_lora(lora_llm, weights_path, end_of_epoch=None)
+
+def read_prompt(prompt, llm=lora_llm, tokenizer=tokenizer, device='cuda'):
+    # Tokenize
+    tokens = tokenizer(
+        prompt, padding=True, return_tensors='pt'
+    )['input_ids'].to(device)
+    
+    # Encode
+    words_embed = llm(
+        tokens, output_hidden_states=True
+    ).hidden_states[-1] # last layer
+
+    return words_embed[:, -1, :] # last or EOS token
+
+
+save_path = './prompts/prompt.pkl'
+def load_pkl(path=save_path):
+    with open(path, "rb") as file:
+        loaded_data = pickle.load(file)
+    return loaded_data
+
+prompt_prev = "INIT"
+prompt_current = None
+embedding = None
+
+try:
+    while(True):
+        print('Checking for change in prompt...')
+        prompt_current = load_pkl()
+        if(prompt_current!=prompt_prev):
+            print('Processing new prompt...')
+            embedding = read_prompt(prompt_current, device='cpu')
+            prompt_prev = prompt_current
+            print(embedding.shape)
+        else:
+            print('Sleep enabled')
+            time.sleep(5)
+            pass
+except KeyboardInterrupt:
+    print('Interrupted program!')
+    sys.exit(0)
+
+
---
+++ subscribe_embedding.py
@@ -0,0 +1,53 @@
+from google.cloud import pubsub_v1
+import sys, os
+import pickle
+# from process_prompt import read_prompt
+
+project_id = "eecse6992-yolov4-tiny-pkk2125"
+topic_name = "LCCE-inference"
+subscription_name = 'LCCE-inference-sub'
+
+
+# Create a Pub/Sub subscriber client
+subscriber = pubsub_v1.SubscriberClient()
+
+# Define the subscription path
+subscription_path = subscriber.subscription_path(project_id, subscription_name)
+prompt = None
+save_path = './prompts/prompt.pkl'
+# Define a message handling function
+def store_pkl(data, path='./prompts/prompt.pkl'):
+    with open(path, "wb") as file:
+        pickle.dump(data, file)
+def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    print(f"Received message: {message.data.decode('utf-8')}")
+    prompt = message.data.decode('utf-8')
+    store_pkl(prompt, save_path)
+    message.ack()
+    # Exit the program after receiving the message
+    os._exit(0)
+
+# Subscribe to the topic and wait for a message
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+
+
+
+
+print(f"Waiting for messages on {subscription_path}...")
+
+try:
+    # Wait for a message to be received or a maximum of 5 seconds
+    streaming_pull_future.result(timeout=5)
+except pubsub_v1.exceptions.TimeoutError:
+    print("No message received within 5 seconds. Exiting...")
+    streaming_pull_future.cancel()
+    os._exit(0)
+except KeyboardInterrupt:
+    streaming_pull_future.cancel()
+    print("Exiting...")
+    os._exit(0)
+
+# print("Generating embedding from extracted prompt...")
+# embedding = read_prompt(prompt)
+# print("Success!")
+
